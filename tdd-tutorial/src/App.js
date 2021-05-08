73997e7b4e7c5d576d02b1424fc37dc67beb28c7
Srim <deamoon0801@naver.com>
2021-05-08 15:29:43 +0900

    tdd-tutorial init

---
+++ tdd-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+      <Profile username="S-rim" name="신서림"/>
+    </>
+  );
+}
+
+export default App;
